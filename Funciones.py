7f018f9e116b44cf9f4291f56cd65ac1c019a200
NazarethMontero <nazareth2792@gmail.com>
2025-11-14 20:18:33 +0100

    Actualizar

--- Funciones.py
+++ Funciones.py
@@ -1,7 +1,8 @@
-#Funciones
+from variables import valores_aleatorio,valores_manual,valores_norte,valores_sur,valores_este,valores_oeste
 import numpy as np
 import random
-
+#Funciones
+#Funciones Naza
 #Crear nuestro tablero:
 
 def crear_tablero(filas=10, columnas=10):
@@ -14,7 +15,7 @@ def crear_tablero_maquina(filas=10, columnas=10):
     tablero_mascara = crear_tablero(filas, columnas)
     return tablero_maquina, tablero_mascara
 
-
+# Funciones Román
 def disparo_maquina(tablero_usuario):
     """
     La máquina dispara aleatoriamente sobre el tablero del usuario.
@@ -50,7 +51,7 @@ def disparo_maquina(tablero_usuario):
     print(f"La máquina ha disparado en ({fila}, {columna}) y ha sido: {resultado.upper()}")
     return fila, columna, resultado
 
-# Función disparo Sara
+# Funciones Sara
 filas = 10
 columnas = 10
 
@@ -108,3 +109,105 @@ def recibir_disparo(fila, col):
         tablero_impactos[fila,col] = mov_agua
         print("Has tocado agua")
         return 0
+
+# Funciones Javier
+
+# Funcion para elegir la modalidad al colocar barcos
+# Manual: El usuario elige la colocación inicial y orientación de cada barco
+# Aleatorio: Se asigna de forma aleatoria la posición de cada barco
+
+def elegir_modalidad():
+    eleccion = input("¿Cómo quieres crear los barcos?\n" \
+    "(Manual (M) o Aleatorio (A))\n")
+    
+    while eleccion not in valores_manual and eleccion not in valores_aleatorio: 
+        eleccion = input("Introduce un valor válido\n" \
+    "(Manual (M) o Aleatorio (A))\n")
+    
+    return eleccion
+
+# Función para crear cada barco en función de su tamaño, estableciendo el punto de inicio del barco
+def generar_barco(eleccion,tamaño):
+    barco = []
+    
+    if eleccion  in valores_manual:
+        print("Has elegido manual.")
+        pos_x = int(input (f"Indícame la fila dónde quieres poner el barco de tamaño {tamaño}:\n"))
+        pos_y = int(input (f"Indícame la columna dónde quieres poner el barco de tamaño {tamaño}:\n"))
+        orientacion = input("Indícame la orientación que quieres que tenga el barco:\n")
+        barco_inicio = np.array([(pos_x,pos_y)])
+        barco = ampliar_barco(orientacion,pos_x,pos_y,barco_inicio,tamaño)
+
+    if eleccion in valores_aleatorio:
+        print("Has elegido aleatorio.")
+        pos_x = np.random.randint(0,10)
+        pos_y = np.random.randint(0,10)
+        barco_inicio = np.array([(pos_x,pos_y)])
+        cardinales = ["N","S","E","O"]
+        orientacion = random.choice(cardinales) # Genero la orientación al azar
+        barco = ampliar_barco(orientacion,pos_x,pos_y,barco_inicio,tamaño)
+
+    return barco
+
+# Función para ir añadiendo casillas al barco hasta completar su tamaño y siguiendo la orientación
+def ampliar_barco(orientacion,x,y,barco,tamaño):
+    if orientacion in valores_norte:
+        for i in range(tamaño-1):
+            x = x -1
+            barco = np.concatenate([(barco),np.array([(x,y)])])
+    elif orientacion in valores_sur:
+        for i in range(tamaño-1):
+            x = x +1
+            barco = np.concatenate([(barco),np.array([(x,y)])])
+    elif orientacion in valores_este:
+        for i in range(tamaño-1):
+            y = y -1
+            barco = np.concatenate([(barco),np.array([(x,y)])])
+    elif orientacion in valores_oeste:
+        for i in range(tamaño-1):
+            y = y +1
+            barco = np.concatenate([(barco),np.array([(x,y)])])
+    return barco
+
+# Función para colocar los barcos generados, comprueba además que no se salga del tablero,
+#  ni coincida o esté próximo a otro barco
+def posicionar_barco(barco, tablero):
+    nuevo_barco = []
+    colocado = False
+    for x,y in barco:
+
+        if not (0 <= x < tablero.shape[0] and 0 <= y < tablero.shape[1]):
+            # raise ValueError
+            print(f"El barco {barco} no puede colocarse fuera del tablero {x,y}")
+            nuevo_barco  = []
+            
+        else: 
+            if tablero[x,y] == 'O':
+                # raise ValueError
+                print(f"No es posible colocar el barco {barco} en una posición ocupada {x,y}")
+                nuevo_barco = []
+                
+            elif comprobar_contiguos(tablero,x,y):
+                print(f"No es posible colocar el barco {barco} en una posición contigua a otro {x,y}")
+                nuevo_barco =[]
+            else:
+                nuevo_barco.append([x,y])
+                
+    print("El barco a pintar es:",nuevo_barco)
+    if nuevo_barco: 
+        colocado = True
+        for x, y in nuevo_barco:
+            tablero[x,y] = 'O'
+    return tablero,colocado
+
+# Función para comprobar las casillas contiguas a una posición x e y dada y ver si tienen valor 'O' de barco
+
+def comprobar_contiguos(tablero, x, y):
+    filas = len(tablero)
+    columnas = len(tablero[0])
+
+    for i in range(max(0, x-1), min(filas, x+2)):
+        for j in range(max(0, y-1), min(columnas, y+2)):
+            if tablero[i][j] == 'O':
+                return True
+    return False
