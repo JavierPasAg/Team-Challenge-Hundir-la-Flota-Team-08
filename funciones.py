ffaa1abb2a0934c72301fd7eeffd8a5c09aab807
JavierPasAg <pascu92@gmail.com>
2025-11-16 20:07:45 +0100

    Mejoras main y funciones

--- funciones.py
+++ funciones.py
@@ -1,6 +1,127 @@
+<<<<<<< Updated upstream
 from variables import valores_aleatorio,valores_manual,valores_norte,valores_sur,valores_este,valores_oeste
 import numpy as np
 import random
+=======
+from variables import valores_aleatorio,valores_manual,valores_norte,valores_sur,valores_este,valores_oeste, mov_tocado,mov_barco,mov_agua, total_casillas
+import numpy as np
+import random
+#Funciones
+#Funciones Naza
+#Crear nuestro tablero:
+
+def crear_tablero(filas=10, columnas=10):
+    return np.full((filas, columnas)," ")
+
+#Crear tablero de la máquina:
+
+def crear_tablero_maquina(filas=10, columnas=10):
+    tablero_maquina = crear_tablero(filas, columnas)
+    tablero_mascara = crear_tablero(filas, columnas)
+    return tablero_maquina, tablero_mascara
+
+# Funciones Román
+def disparo_maquina(tablero_usuario, fin_partida):
+    """
+    La máquina dispara aleatoriamente sobre el tablero del usuario.
+    Si cae en una casilla ya disparada, muestra mensaje y vuelve a intentar.
+    """
+    
+    nfil, ncol = tablero_usuario.shape
+    acierto = False
+    while True:
+        fila = random.randrange(nfil)
+        columna = random.randrange(ncol)
+        
+        celda = tablero_usuario[fila, columna]
+        
+        # 1. Si ya se disparó ahí → mensaje y continuar
+        if celda in ("X", "-"):
+            print(f"La máquina intentó disparar en ({fila}, {columna}) pero YA había disparado ahí.")
+            continue
+        
+        # 2. Si hay barco → tocado
+        if celda == "O":
+            tablero_usuario[fila, columna] = "X"
+            resultado = "tocado"
+            acierto = True
+        # 3. Si hay agua
+        elif celda == " ":
+            tablero_usuario[fila, columna] = "-"
+            resultado = "agua"
+        
+        # 4. Ya tenemos un disparo válido, salir del bucle
+        break
+    
+    print(f"La máquina ha disparado en ({fila}, {columna}) y ha sido: {resultado.upper()}")
+    conteo = np.count_nonzero(tablero_usuario == 'X')
+    if conteo == total_casillas:
+        fin_partida = True
+
+    return tablero_usuario, acierto, fin_partida
+
+# Funciones Sara
+filas = 10
+columnas = 10
+
+#Turno completo del usuario
+#Pide las coordenadas al usuario
+#Llama al tablero de la maquina para recibir el disparo
+#Si dispara a un barco (la funcion devuelve 1), el usuario repite turno
+#Si dispara al agua (la funcion devuelve 0), finaliza su turno y es el turno de la maquina
+#Si ya ha disparado en esa celda (la funcion devuelve -1) y se le vuelven a pedir coordenadas al usuario
+
+
+def pedir_coordenadas_usuario():
+ 
+    #Pide por teclado una fila y una columna válidas al usuario.
+    #Devuelve (fila, col) como enteros.
+    while True:
+        try:
+            fila = int(input(f"Introduce la FILA (0 - {filas - 1}): "))
+            col = int(input(f"Introduce la COLUMNA (0 - {columnas- 1}): "))
+
+            if 0 <= fila < filas and 0 <= col < columnas:
+                return fila, col
+            else:
+                print("Coordenadas fuera del tablero. Inténtalo de nuevo.\n")
+        except ValueError:
+            print("Debes introducir NÚMEROS enteros. Inténtalo de nuevo.\n")
+    
+    return fila,col
+
+def recibir_disparo(fila, col,tablero_maquina,tablero_mascara, fin_partida):
+
+    #Devuelve:
+    # -1 -> ya se había disparado en esa casilla
+    # "1" -> si impacta en un barco
+    # "0" -> si disparo se efectua en el agua
+    acierto = False
+    #Comprobamos si se ha realizado un disparo en una coordenada
+    if tablero_mascara[fila,col] in (mov_tocado,mov_agua):
+        print("Ya se habia disparado aqui")
+        
+    
+    #Si hay un barco
+    if tablero_maquina[fila,col] == mov_barco:
+        tablero_maquina[fila,col] = mov_tocado #Marco el barco tocado en el tablero de los barcos
+        tablero_mascara[fila,col] = mov_tocado #Marco el barco tocado en el tablero de los impactos
+        acierto = True
+        print ("Tocado!")
+        # return 1
+
+    #Si hay agua
+    else:
+        tablero_mascara[fila,col] = mov_agua
+        print("Has tocado agua")
+    conteo = np.count_nonzero(tablero_mascara == 'X')
+    if conteo == total_casillas:
+        fin_partida = True
+
+    return tablero_maquina,tablero_mascara,acierto,fin_partida
+    
+# Funciones Javier
+>>>>>>> Stashed changes
 
 # Funcion para elegir la modalidad al colocar barcos
 # Manual: El usuario elige la colocación inicial y orientación de cada barco
@@ -70,16 +191,19 @@ def posicionar_barco(barco, tablero):
             # raise ValueError
             print(f"El barco {barco} no puede colocarse fuera del tablero {x,y}")
             nuevo_barco  = []
+            break
             
         else: 
-            if tablero[x,y] == 'O':
+            if tablero[x,y] == mov_barco:
                 # raise ValueError
                 print(f"No es posible colocar el barco {barco} en una posición ocupada {x,y}")
                 nuevo_barco = []
+                break
                 
             elif comprobar_contiguos(tablero,x,y):
                 print(f"No es posible colocar el barco {barco} en una posición contigua a otro {x,y}")
                 nuevo_barco =[]
+                break
             else:
                 nuevo_barco.append([x,y])
                 
@@ -87,7 +211,7 @@ def posicionar_barco(barco, tablero):
     if nuevo_barco: 
         colocado = True
         for x, y in nuevo_barco:
-            tablero[x,y] = 'O'
+            tablero[x,y] = mov_barco
     return tablero,colocado
 
 # Función para comprobar las casillas contiguas a una posición x e y dada y ver si tienen valor 'O' de barco
@@ -98,6 +222,6 @@ def comprobar_contiguos(tablero, x, y):
 
     for i in range(max(0, x-1), min(filas, x+2)):
         for j in range(max(0, y-1), min(columnas, y+2)):
-            if tablero[i][j] == 'O':
+            if tablero[i][j] == mov_barco:
                 return True
     return False
--- variables.py
+++ variables.py
@@ -1,4 +1,45 @@
+<<<<<<< Updated upstream
 # constantes
+=======
+#Variables
+
+bienvenida = """¡BIENVENIDO A HUNDIR LA FLOTA!\n
+1.Tu tablero y el de la máquina son de 10x10 casillas.\n
+2. Hay distintos barcos que debes hundir:\n
+   - 4 barcos de 1 casilla\n
+   - 3 barcos de 2 casillas\n
+   - 2 barcos de 3 casillas\n
+   - 1 barco de 4 casillas\n
+3. Cada turno, dispara a una coordenada (fila,columna). Ejemplo: 3,5\n
+   - Si aciertas, tu turno continúa.\n
+   - Si fallas, le toca a la máquina.\n
+4. El juego termina cuando un jugador pierde todos sus barcos.\n
+¡Buena suerte!"""
+
+fin_partida = False
+victoria = """¡Enhorabuena!\n
+Has ganado la partida\n
+Tú eres el verdadero máquina"""
+derrota = """¡Vaya!, Has perdido la partida.\n
+Vuelve a intentarlo"""
+usuario1 = "Máquina"
+usuario2 = "Yo"
+
+tablero = n_filas = 10; n_columnas = 10
+
+#Barcos
+#b1(tamaño 1)= 4 barcos
+#b2 (tamaño 2)= 3 barcos
+#b3 (tamaño 3)= 2 barcos
+#b4 (tamaño 4) = 1 barco
+
+barcos = [4,3,3,2,2,2,1,1,1,1]
+total_casillas = 20
+mov_barco = "O"
+mov_tocado = "X"
+mov_agua = "-"
+
+>>>>>>> Stashed changes
 valores_manual = ["M","m","Manual","manual","MANUAL"]
 valores_aleatorio = ["A","a","Aleatorio","aleatorio","ALEATORIO"]
 valores_norte = ["N","n","Norte","norte","NORTE"]
