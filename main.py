9478cd9a6b8d2beda59c74424d3e8844c892e468
sergidelacruz <sergidelacruz1994@gmail.com>
2025-11-18 22:46:35 +0100

    mejoras 251118

---
+++ main.py
@@ -0,0 +1,88 @@
+from funciones import (
+    generar_barco,posicionar_barco,elegir_modalidad,crear_tablero,
+    crear_tablero_maquina, pedir_coordenadas_usuario, recibir_disparo,
+    disparo_maquina
+)
+from variables import bienvenida, fin_partida, victoria, derrota, barcos
+import numpy as np
+
+# tablero_test = np.full((10,10)," ")
+# EJECUTAR CON: python main.py
+print(bienvenida)
+
+tablero_usuario = crear_tablero()
+tablero_maquina, tablero_mascara = crear_tablero_maquina()
+print("Tablero usuario\n",tablero_usuario)
+print("Tablero enemigo:\n", tablero_mascara)
+# Usuario elige y colca barcos
+eleccion = elegir_modalidad()
+for tamaño in [4,3,3,2,2,2,1,1,1,1]:
+    colocado = False
+    while not colocado:
+        barco = (generar_barco(eleccion,tamaño))
+        print(barco)
+        tablero,colocado = posicionar_barco(barco,tablero_usuario)
+
+
+# Máquina coloca barcos
+eleccion_maquina = "A"
+for tamaño in [4,3,3,2,2,2,1,1,1,1]:
+    colocado = False
+    while not colocado:
+        barco = (generar_barco(eleccion_maquina,tamaño))
+        print(barco)
+        tablero_maquina,colocado = posicionar_barco(barco,tablero_maquina)
+
+print("Tablero usuario:\n",tablero)
+print("Tablero trampa:\n", tablero_maquina)
+print("Tablero enemigo:\n",tablero_mascara)
+
+def separador(titulo=None):
+    print("\n" + "=" * 60)
+    if titulo:
+        print(f" {titulo}")
+        print("-" * 60)
+    print()
+    
+# BUCLE CON ESTO
+# Pedimos coordenadas
+while not fin_partida:
+    fin_partida
+    acierto_usuario = True
+    acierto_maquina = True
+    # Bucle que se repite mientras acierte el usuario
+    while acierto_usuario:
+        separador("TU TURNO")
+        fila_elegida, columna_elegida = pedir_coordenadas_usuario()
+        print(fila_elegida,columna_elegida)
+        tablero_maquina,tablero_mascara, acierto_usuario, fin_partida = recibir_disparo(fila_elegida,columna_elegida,tablero_maquina,tablero_mascara, fin_partida)
+        print("""
+              """)
+        print("Tablero usuario:\n",tablero)
+        print("Tablero trampa:\n", tablero_maquina)
+        print("Tablero enemigo:\n",tablero_mascara)
+        if fin_partida:
+            print(victoria)
+            break
+    
+    # Evaluamos si ha terminado la partida
+    if fin_partida:
+        break
+    # Bucle que se repite mientras acierte la máquina
+    while acierto_maquina:
+        separador("TURNO DE LA MÁQUINA")
+        tablero_usuario,acierto_maquina, fin_partida = disparo_maquina(tablero_usuario, fin_partida)
+        print("""
+              """)
+        print("Tablero usuario:\n",tablero)
+        print("Tablero trampa:\n", tablero_maquina)
+        print("Tablero enemigo:\n",tablero_mascara)
+        if fin_partida:
+            print(derrota)
+            break
+    # Evaluamos si ha terminado la partida 
+    if fin_partida:
+        break
+
+
+
---
+++ variables.py
@@ -0,0 +1,53 @@
+#Variables
+
+bienvenida = r"""
+¡BIENVENIDO A HUNDIR LA FLOTA!
+
+                __/___            
+          _____/______|           
+  _______/_____\_______\_____     
+  \              < < <       |    
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+1. Tu tablero y el de la máquina son de 10x10 casillas.
+2. Hay distintos barcos que debes hundir:
+   - 4 barcos de 1 casilla 
+   - 3 barcos de 2 casillas 
+   - 2 barcos de 3 casillas 
+   - 1 barco de 4 casillas 
+3. Cada turno, dispara a una coordenada (fila,columna). Ejemplo: 3,5
+   - Si aciertas, tu turno continúa.
+   - Si fallas, le toca a la máquina.
+4. El juego termina cuando un jugador pierde todos sus barcos.
+¡Buena suerte!
+
+"""
+fin_partida = False
+victoria = """¡Enhorabuena!\n
+Has ganado la partida\n
+Tú eres el verdadero máquina"""
+derrota = """¡Vaya!, Has perdido la partida.\n
+Vuelve a intentarlo"""
+usuario1 = "Máquina"
+usuario2 = "Yo"
+
+tablero = n_filas = 10; n_columnas = 10
+
+#Barcos
+#b1(tamaño 1)= 4 barcos
+#b2 (tamaño 2)= 3 barcos
+#b3 (tamaño 3)= 2 barcos
+#b4 (tamaño 4) = 1 barco
+
+barcos = [4,3,3,2,2,2,1,1,1,1]
+total_casillas = 20
+mov_barco = "O"
+mov_tocado = "X"
+mov_agua = "-"
+
+valores_manual = ["M","m","Manual","manual","MANUAL"]
+valores_aleatorio = ["A","a","Aleatorio","aleatorio","ALEATORIO"]
+valores_norte = ["N","n","Norte","norte","NORTE"]
+valores_sur = ["S","s","Sur","sur","SUR"]
+valores_este = ["E","e","Este","este","ESTE"]
+valores_oeste = ["O","o","Oeste","oeste","OESTE"]
